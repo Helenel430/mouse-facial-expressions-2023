88d7a513ef681a257ef1b014c3f3affc80b55448
Andre Telfer <andretelfer@cmail.carleton.ca>
2023-06-27 11:00:32 -0700

    renamed project

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='mouse_facial_expressions_2023',
+    name='mouse_facial_expressions',
     packages=find_packages(),
     version='0.1.0',
     description='A short description of the project.',
